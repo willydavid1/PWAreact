14ac42f3cec0c8bc8190b8761ec08c28e08d5785
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-08-02 17:11:30 -0500

    Aplicando estrategias de carga

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,6 +11,23 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
 workbox.routing.registerNavigationRoute('/index.html')
 
+// La API usa la estategia Stale While Revalidate para mayor velocidad
+workbox.routing.registerRoute(/^https?:\/\/www.themealdb.com\/api\/.*/, 
+    workbox.strategies.staleWhileRevalidate(),
+    'GET')
+
+// La fuente usa la estategia cacheFirst
+workbox.routing.registerRoute(/^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/,
+    workbox.strategies.cacheFirst({
+        cacheName: 'google-fonts-cacheNAME',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 24 * 60 * 60
+            })
+        ]
+    }),
+    'GET')
+
 // aplicamos el Network First, para cualquier ruta que empieze con http o https, va aplicar una estrategia networkFirst, (cachea peticiones GET) | va a tratar a ir a red si no hay conexion retorna lo que hay en cache | las dos de abajo es la manera mas nueva segun la DOC
 workbox.routing.registerRoute(/^https?.*/,
     workbox.strategies.networkFirst(), 'GET')
